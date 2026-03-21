5d3824271df1c2260b4f22282a0637d546ed7788
Jeff Gentapanan <jeff.gentapanan2004525@gmail.com>
2026-03-21 15:12:27 +0800

    changes

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 
 // 1. Point to the correct build folder (usually '../client/dist')
-app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 // 2. API routes go first
 const router = require("./routes");
@@ -16,7 +16,7 @@ app.use("/api", router);
 // 3. Use a Regex to match all routes EXCEPT those starting with /api
 // This prevents the PathError and serves your React app correctly
 app.get(/^(?!\/api).+/, (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 const port = process.env.PORT || 5000;
